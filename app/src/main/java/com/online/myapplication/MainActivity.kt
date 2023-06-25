db97674930ecc897fa79dfbd8d4a4f18e04cab5d
Alex <76860596+ALEX5402@users.noreply.github.com>
2023-06-25 10:21:40 +0530

    bug fixed

--- app/src/main/java/com/online/myapplication/MainActivity.kt
+++ app/src/main/java/com/online/myapplication/MainActivity.kt
@@ -1,57 +1,50 @@
 package com.online.myapplication
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.online.myapplication.databinding.ActivityMainBinding
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
 
     lateinit var activityMainBinding: ActivityMainBinding
+    @SuppressLint("SuspiciousIndentation", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
         val viewmodel = ViewModelProvider(this).get(viewmodel::class.java)
 
-
-
-
-
-        activityMainBinding.mainstring.isTextInputLayoutFocusedRectEnabled
-            .apply {
-                activityMainBinding.mainstring.text .let {
-                    viewmodel.mainstring = it.toString()
-                    Log.w("jajaj",viewmodel.mainstring)
-                }
-            }
-
-
         activityMainBinding.encriptbutton.setOnClickListener {
 
        val lenth = activityMainBinding.mainstring.text
+
             if (lenth?.isBlank()!!)
             {
                 Toast.makeText(this,"please add text " ,Toast.LENGTH_LONG).show()
                 activityMainBinding.outout.text = ""
-            } else{
 
+
+            } else{
+                Log.w("AAAA",lenth.toString())
                 val alexEnc = AlexEnc()
                 try {
-                    val output  =   alexEnc.encryptString(viewmodel.mainstring)
+                    val output  =   alexEnc.encryptString(lenth.toString())
                     activityMainBinding.outout.text = "Enc String is"  + output.first
-                    activityMainBinding.outout.text = "Enc Key is"  +  output.second
+
                     viewmodel.enckey = output.second
                     viewmodel.encstring = output.first
+                    Log.w("AAAA",output.first)
+                    Log.w("AAAA",output.second)
+                    val decriptedkey = alexEnc.decryptString(viewmodel.encstring,viewmodel.enckey)
+                    activityMainBinding.outout2.text =  "main string is : $decriptedkey"
                 }catch (err : Exception)
                 {
                     err.printStackTrace()
@@ -65,12 +58,10 @@ class MainActivity : AppCompatActivity() {
             val clipData = ClipData.newPlainText("Text",viewmodel.encstring)
              clipboardManager.setPrimaryClip(clipData)
         }
-
     activityMainBinding.key.setOnClickListener {
             val clipboardManager = this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val clipData = ClipData.newPlainText("Text",viewmodel.enckey)
              clipboardManager.setPrimaryClip(clipData)
         }
-
     }
 }
--- app/src/main/java/com/online/myapplication/viewmodel.kt
+++ app/src/main/java/com/online/myapplication/viewmodel.kt
@@ -3,8 +3,6 @@ package com.online.myapplication
 import androidx.lifecycle.ViewModel
 
 class viewmodel : ViewModel(){
-    var mainoutput : String = ""
-    var mainstring  : String = ""
     var encstring  : String = ""
     var enckey  : String = ""
 }
